fc50e925332b8593a938d77d619a50f00a8e694a
Claes Jacobsson <claes.jacobsson@gmail.com>
2019-04-24 23:31:44 +0200

    New function to add border to squircle

--- Squircle/Squircle.swift
+++ Squircle/Squircle.swift
@@ -16,6 +16,13 @@ extension UIView {
     public func squircle() {
         self.layer.applySquircle()
     }
+    
+    /**
+     Apply squircle corner radius and a border.
+     */
+    public func squircleWithBorder(width: CGFloat = 1, color: UIColor = UIColor.init(white: 0.9, alpha: 1.0)) {
+        self.layer.applySquircleWithBorder(width: width, color: color)
+    }
 }
 
 extension CALayer {
@@ -27,5 +34,19 @@ extension CALayer {
         let maskLayer = CAShapeLayer()
         maskLayer.path = squirclePath.cgPath
         self.mask = maskLayer
+    }
+    
+    /**
+     Apply a squircle mask corner radius and a border.
+     */
+    public func applySquircleWithBorder(width: CGFloat, color: UIColor) {
+        applySquircle()
+        let borderLayer = CAShapeLayer()
+        borderLayer.path = squirclePath.cgPath
+        borderLayer.lineWidth = width
+        borderLayer.strokeColor = color.cgColor
+        borderLayer.fillColor = UIColor.clear.cgColor
+        borderLayer.frame = self.bounds
+        self.addSublayer(borderLayer)
     }
 }
