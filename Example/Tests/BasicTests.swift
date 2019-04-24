026492daed641a263d7144ecdde0bb7641dbb16f
Giuseppe Travasoni <giuseppe.travasoni@gmail.com>
2019-04-24 14:49:17 +0200

    points test

--- Example/Tests/BasicTests.swift
+++ Example/Tests/BasicTests.swift
@@ -5,7 +5,20 @@ class BasicTests: XCTestCase {
     
     func testImplementation() {
         let view = UIView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
-        let path = view.layer.squirclePath()
+        let path = view.layer.squirclePath
+        XCTAssertNotNil(path)
+    }
+    
+    func testPoints() {
+        let view = UIView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+        let path = view.layer.squirclePath
+        
+        let zeroPoint = CGPoint.zero
+        XCTAssertFalse(path.contains(zeroPoint))
+        
+        let midPoint = view.center
+        XCTAssertTrue(path.contains(midPoint))
+        
         XCTAssertNotNil(path)
     }
 }
