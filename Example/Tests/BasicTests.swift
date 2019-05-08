28650f8c60f07c68fd58c6cbab50d0b49cfaaee7
Claes Jacobsson <claes.jacobsson@gmail.com>
2019-05-08 12:52:59 +0200

    Simple test for add/remove border

--- Example/Tests/BasicTests.swift
+++ Example/Tests/BasicTests.swift
@@ -21,4 +21,22 @@ class BasicTests: XCTestCase {
         
         XCTAssertNotNil(path)
     }
+    
+    func testBorderImplementation() {
+        let view = UIView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+        view.squircleWithBorder(width: 5.0, color: .orange)
+        
+        // Test that we have a border
+        var count = borderCount(view: view)
+        XCTAssertTrue(count == 1)
+        
+        // Test that we can remove border
+        view.removeSquircleBorder()
+        count = borderCount(view: view)
+        XCTAssertTrue(count == 0)
+    }
+    
+    func borderCount(view: UIView) -> Int {
+        return view.layer.sublayers?.filter({ $0.name == squircleBorderName }).count ?? 0
+    }
 }
